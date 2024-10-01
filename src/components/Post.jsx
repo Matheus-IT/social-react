2442eb34ca8e013c24a19b7693e5375879dec87b
matheus <matheuscosta.tech@gmail.com>
2024-09-30 22:56:31 -0300

    Rendering dynamic value

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
 export default function Post() {
+  const names = ["matheus", "carol"];
+  const name = names[Math.floor(Math.random() * names.length)];
   return (
     <div>
-      <p>Matheus</p>
+      <p>{name}</p>
       <p>React.js is awesome</p>
     </div>
   );
