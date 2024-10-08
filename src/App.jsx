281a5a7ff3e6ae2a36d112a9d5504ea27f4f2156
matheus <matheuscosta.tech@gmail.com>
2024-10-07 22:16:18 -0300

    Playing around with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import NewPost from "./components/NewPost";
 import PostList from "./components/PostList";
 
 export default function App() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onChangeBody={changeBodyHandler}
+        onChangeAuthor={changeAuthorHandler}
+      />
 
       <PostList
         posts={[
+          { author: enteredAuthor, body: enteredBody },
           { author: "matheus", body: "first post" },
           { author: "carol", body: "second post" },
         ]}
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,15 @@
 import classes from "./NewPost.module.css";
 
-export default function NewPost() {
+export default function NewPost(props) {
   return (
     <form className={classes.form}>
       <div>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onChangeBody} />
       </div>
       <div>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onChangeAuthor} />
       </div>
     </form>
   );
